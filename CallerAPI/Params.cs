19db64f0ee84957501b5029979eacddce83a929a
eduard_nicolae <eduard_nicolae@yahoo.com>
2020-05-26 15:36:22 +0300

    Params for request web servie.

---
+++ CallerAPI/Params.cs
@@ -0,0 +1,25 @@
+﻿using System.Net;
+
+namespace CallerAPI
+{
+    public class Params
+    {
+        // URI request.
+        public string URI { internal get; set; }
+
+        // Method request.
+        public string Method { internal get; set; }
+
+        // Result status code request.
+        public HttpStatusCode HttpStatusCode { internal get; set; } = HttpStatusCode.OK;
+
+        // Headers request.
+        public WebHeaderCollection Headers { internal get; set; } = new WebHeaderCollection();
+
+        // Contnt request.
+        public byte[] Content { internal get; set; } = new byte[] { };
+
+        // Content type request.
+        public string ContentType { internal get; set; }
+    }
+}
